8710e18ee9fa0752efce9306db224bb9ed1063fc
HatemHashem <hatemsayed001@gmail.com>
2021-04-03 22:39:18 +0200

    improve the form date

--- Project/forms/project_form.py
+++ Project/forms/project_form.py
@@ -1,9 +1,14 @@
 from django import forms
 from Project.models.user_project import UserProject
+from django.contrib.admin import widgets
 
+class DateInput(forms.DateInput):
+    input_type='date'
 class ProjectForm(forms.ModelForm):
 
     class Meta:
         model=UserProject
         fields=("title","details","total_target","start_date","end_date","category")
-        #test
+        widgets={'start_date':DateInput(),'end_date':DateInput()}
+       
+
