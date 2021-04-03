b8b758ccc2e65a2ba4fb2479afbdb8300104f249
Maymaher <mai.maher003@gmail.com>
2021-04-03 17:37:36 -0400

    create user app and login page

---
+++ User/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path
+from django.urls import path,include
+# from .views import crowd_project
+
+urlpatterns = [
+    # path('', include('User.urls')),
+   
+   
+]
--- crowdFunding/urls.py
+++ crowdFunding/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Project.urls')),
+    path('User/',include('django.contrib.auth.urls')),
+    path('User/', include('User.urls')),
 ]
