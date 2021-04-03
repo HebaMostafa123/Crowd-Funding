ea062766c8d03aa3e3519e4a08f6c80b2829529a
Heba Mostafa <Hoba.mostafa97@gmail.com>
2021-04-03 22:49:05 +0200

    change from user model to auth-user model

--- Project/models/user.py
+++ Project/models/user.py
@@ -1,14 +1,14 @@
-from django.db import models
-from datetime import datetime
+# from django.db import models
+# from datetime import datetime
 
 
-class CrowdUser(models.Model):
-        first_name = models.CharField(max_length=100)
-        last_name = models.CharField(max_length=100)
-        email = models.EmailField(max_length=200)
-        password = models.CharField(max_length=100)
-        phone = models.CharField(max_length=100)
-        profile_picture = models.ImageField(blank=True,upload_to = 'images/user/')
-        created_at = models.DateTimeField(default=datetime.now, blank=True)
-        updated_at = models.DateTimeField()
+# class CrowdUser(models.Model):
+#         first_name = models.CharField(max_length=100)
+#         last_name = models.CharField(max_length=100)
+#         email = models.EmailField(max_length=200)
+#         password = models.CharField(max_length=100)
+#         phone = models.CharField(max_length=100)
+#         profile_picture = models.ImageField(blank=True,upload_to = 'images/user/')
+#         created_at = models.DateTimeField(default=datetime.now, blank=True)
+#         updated_at = models.DateTimeField()
 
