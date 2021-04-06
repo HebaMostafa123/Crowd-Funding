cc4b44cf14eac7e90a1715bc03b34af2de85cecc
Atef <mahmoudbenatef@gmail.com>
2021-04-06 18:48:36 +0200

    add comment report

--- Project/urls.py
+++ Project/urls.py
@@ -8,12 +8,13 @@ from .views import showProject
 urlpatterns = [
     # path('', include('Project.urls')),
     path('home',crowd_project.index),
-    path('project/list',crowd_project.project_list),
+    path('project/list',crowd_project.project_list ,name='list'),
     path('project/form',crowd_project.project_form),
     url(r'^project/(?P<project_id>\d+)/$', showProject.show, name='item_read'),
     path('project/comment', showProject.comment, name='comment'),
     url('project/rate', showProject.rate, name='rate'),
     url('project/report', showProject.report, name='report-project'),
+    url('comment/report', showProject.reportComment, name='report-comment'),
 
     url(r'^project/(?P<project_id>\d+)/edit$', crowd_project.edit, name='project_edit'),
     url(r'^project/(?P<project_id>\d+)/update$', crowd_project.update, name='project_update'),
