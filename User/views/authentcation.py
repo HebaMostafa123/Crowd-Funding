e9c24a79c1d6b8de7048e6576af546635e6c1ddc
Atef <mahmoudbenatef@gmail.com>
2021-04-09 20:42:57 +0200

    add login redirection and add donate percentage per project

--- User/views/authentcation.py
+++ User/views/authentcation.py
@@ -145,7 +145,8 @@ class ActivateAccountView(View):
 
 
 def loginPage(request):
-
+    if  request.user.is_authenticated:
+        return redirect('/')
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
